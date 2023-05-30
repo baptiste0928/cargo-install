86b92cc607631d4be9fc0db814927066e1e68ac5
Baptiste Girardeau <contact@bgirardeau.me>
2023-05-30 22:52:55 +0200

    feat: add git action input parameters

--- src/parse.ts
+++ src/parse.ts
@@ -4,36 +4,70 @@ import stringArgv from 'string-argv'
 
 export interface ActionInput {
   crate: string
-  version: string
+  source: CratesIoSource | GitSource
   features: string[]
   args: string[]
   cacheKey: string
 }
 
+export interface CratesIoSource {
+  version: string
+}
+
+export interface GitSource {
+  repository: string
+  branch?: string
+  tag?: string
+  rev?: string
+}
+
 // Parse and validate action input
 export function parseInput (): ActionInput {
+  // Global parameters
   const crate = core.getInput('crate', { required: true })
-  const version = core.getInput('version', { required: true })
   const features = core.getInput('features', { required: false })
   const locked = core.getBooleanInput('locked', { required: false })
   const args = core.getInput('args', { required: false })
   const cacheKey = core.getInput('cache-key', { required: false })
 
+  const parsedArgs = stringArgv(args)
+  const parsedFeatures = features.split(/[ ,]+/).filter(Boolean)
+  if (locked) {
+    parsedArgs.push('--locked')
+  }
+
+  // Crates.io version (always provided, defaults to 'latest')
+  const version = core.getInput('version', { required: true })
   if (version !== 'latest' && semver.validRange(version) === null) {
     core.setFailed('Invalid version provided. Must be a valid semver range or "latest".')
     process.exit(1)
   }
 
-  const parsedArgs = stringArgv(args)
-  const parsedFeatures = features.split(/[ ,]+/).filter(Boolean)
+  // Git source (optional, overrides Crates.io version if provided)
+  const repository = core.getInput('git', { required: false })
+  const branch = core.getInput('branch', { required: false })
+  const tag = core.getInput('tag', { required: false })
+  const rev = core.getInput('rev', { required: false })
 
-  if (locked) {
-    parsedArgs.push('--locked')
+  let source: CratesIoSource | GitSource = { version }
+  if (repository !== '') {
+    source = { repository }
+    source.branch = branch !== '' ? branch : undefined
+    source.tag = tag !== '' ? tag : undefined
+    source.rev = rev !== '' ? rev : undefined
+  }
+
+  // Warnings if both crates.io and git are provided
+  if (repository === '' && (branch !== '' || tag !== '' || rev !== '')) {
+    core.warning('Ignoring branch, tag, and rev since git is not provided.')
+  }
+  if (repository !== '' && version !== 'latest') {
+    core.warning('Ignoring version since git is provided.')
   }
 
   return {
     crate,
-    version,
+    source,
     features: parsedFeatures,
     args: parsedArgs,
     cacheKey
